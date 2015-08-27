93dfa9dd1c1cd28600c347358a3d2e6e5ed909b0
jinyu112 <jinyuguan1102@gmail.com>
2015-08-27 18:03:51 -0400

    issue 5: introduced bar and pie charts for data visualization, introduced integer formatter

---
+++ app/src/main/java/example/org/bjjanatest/IntValueFormatter.java
@@ -0,0 +1,17 @@
+package example.org.bjjanatest;
+
+import com.github.mikephil.charting.utils.ValueFormatter;
+//this class formats the pie chart values are integers
+
+public class IntValueFormatter implements ValueFormatter {
+
+    public IntValueFormatter() {
+    }
+
+    @Override
+    public String getFormattedValue(float value) {
+        int tempInt;
+        tempInt = Math.round(value);
+        return Integer.toString(tempInt);
+    }
+}
