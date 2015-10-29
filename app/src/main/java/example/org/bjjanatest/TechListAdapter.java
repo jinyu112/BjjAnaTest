1258cd3990e8f632f20f4cb1690eaf5d846e7287
jinyu112 <jinyuguan1102@gmail.com>
2015-10-28 20:44:07 -0400

    Issue 25: added technique icons

--- app/src/main/java/example/org/bjjanatest/TechListAdapter.java
+++ app/src/main/java/example/org/bjjanatest/TechListAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.List;
 
@@ -34,23 +35,32 @@ public class TechListAdapter extends ArrayAdapter<Tech> {
         tv.setText(tech.getTechName());
 
         tv = (MyTextView) view.findViewById(R.id.tech_list_item_cat);
+        ImageView iv = (ImageView) view.findViewById(R.id.tech_list_item_icon);
+
+
         if (tech.getTechType()==0) {
             tv.setText("Position");
+            iv.setImageResource(R.drawable.position_50);
         }
         else if (tech.getTechType()==1) {
             tv.setText("Submission");
+            iv.setImageResource(R.drawable.submission__50);
         }
         else if (tech.getTechType()==2) {
             tv.setText("Sweep");
+            iv.setImageResource(R.drawable.sweep_50);
         }
         else if (tech.getTechType()==3) {
             tv.setText("Takedown");
+            iv.setImageResource(R.drawable.td_50);
         }
         else if (tech.getTechType()==4) {
             tv.setText("Reversal");
+            iv.setImageResource(R.drawable.reversal_50);
         }
         else if (tech.getTechType()==5) {
             tv.setText("Defense");
+            iv.setImageResource(R.drawable.defense_50);
         }
         else {
             tv.setText("Other");
